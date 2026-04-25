5c9ec235b8059647dca030133128564dbc038741
ncuelan <ncuelan@gmail.com>
2026-04-25 09:57:39 +0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // 如果部署在 GitHub Pages (網址後綴為 /pixelgame/)，請使用 repo 名稱：
+  base: '/pixel-game/',
+  // 如果是部署在 Vercel 或 Netlify (網址在根目錄 /)，請改為：
+  // base: '/',
+})
